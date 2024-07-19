6952ec25001d346cd72e8a1fedb8880910a97d0c
xtc2016xtc <728611827@qq.com>
2024-07-19 17:14:22 +0800

    公共样式

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,14 @@
-
+import styles,{layout} from "../style"
 const Navbar = () => {
   return (
-    <div className="text-red">
-      你好
+    <div className="flex flex-1 flex-wrap font-poppins font-normal sm:flex-grow">
+      <h1 className={`${styles.button} text-ellipsis text-wrap`}>
+        <ul className={layout.section}>
+          <li>
+            nh1
+          </li>
+        </ul>
+      </h1>
     </div>
   )
 }
---
+++ src/components/style.ts
@@ -0,0 +1,12 @@
+//公共样式
+export const styles = {
+  headers:"text-white font-bold text-2xl",
+  body:"text-white font-bold text-xl",
+  footer:"text-white font-bold text-lg",
+  button:"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+}
+export const layout = {
+  section:`${styles.body} flex`
+}
+
+export default styles
