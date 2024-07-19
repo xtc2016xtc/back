371d4bacb49ef656800958a6a951db76b94436c3
xtc2016xtc <728611827@qq.com>
2024-07-19 14:14:32 +0800

    正式初始化

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './globals.css'
+const App = () => {
+ return (
+    <div className="home-posts text-3xl font-bold underline text-red">app</div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App';
+import './globals.css';
+ReactDom.createRoot(document.getElementById('root')!).render(<App />);
