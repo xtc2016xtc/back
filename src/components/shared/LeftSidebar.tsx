c9e8107bf532fc479b7bed4fa2348ea5ae6b2a96
xtc2016xtc <728611827@qq.com>
2024-08-01 17:09:28 +0800

    导航栏

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+//2.26.13 import React from 'react'
+
+import { logoImg } from "@/utils"
+import { Link } from "react-router-dom"
 
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <nav className="leftSidebar">
+      <div className="flex flex-col gap-11">
+      <Link to="/" className='flex items-center gap-3'>
+        <img 
+          src={logoImg} 
+          alt="logo"
+          width={170}
+          height={36}
+          />
+      </Link>
+
+      <Link 
+        to={``}
+      >
+      </Link>
+      </div>
+    </nav>
   )
 }
 
